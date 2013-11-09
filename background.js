9214a21ead8fdd598efec57654c88541928a194c
Dmitriy Pyankov <mail@zerodi.ru>
2013-11-10 01:55:06 +0600

    v0.2.5. First Release.

--- background.js
+++ background.js
@@ -6,98 +6,115 @@ chrome.contextMenus.create({
     "type": "normal",
     "contexts": ["all"],
     "documentUrlPatterns": ["*://vk.com/*"],
-    "onclick": loadEventInfo
+    "onclick": getOnClick()
 });
 
-function loadEventInfo() {
+function getOnClick() {
+    /**
+     * Функция возвращает окончательное значение getOnClick
+     */
+    return function (info, tab) {
+        //Объявляем переменные для приложения и авторизации
+        var eventUrl = info.srcUrl,
+            infoTabUrl = 'sendevent.html#',
+            vkAppID = '3965536',
+            vkScopes = 'groups,offline',
+            vkRedirectUri = encodeURIComponent("http://oauth.vk.com/blank.html"),
+            vkAuthUrl = 'https://oauth.vk.com/authorize?client_id=' + vkAppID +
+                '&scope=' + vkScopes +
+                '&redirect_uri=' + vkRedirectUri +
+                '&display=page&response_type=token';
+        //Вызываем токен из хранилища
+        chrome.storage.local.get({'vkaccess_token': {}}, function (items) {
+            //Проверяем наличие токена, если его нет, то получаем.
+            if (items.vkaccess_token.length === undefined) {
+                chrome.tabs.create({url: vkAuthUrl, selected: true}, function (tab) {
+                    chrome.tabs.onUpdated.addListener(authListener(tab.id, eventUrl));
+                });
 
-    // Do NOT forget that the method is ASYNCHRONOUS
-    chrome.tabs.query({
-        active: true,               // Select active tabs
-        lastFocusedWindow: true     // In the current window
-    }, function(Tabs) {
-        // Since there can only be one active tab in one active window,
-        //  the array has only one element
-        var tab = Tabs[0];
-        var eventUrl = tab.url;
-        var getEventRequest = new XMLHttpRequest(),
-            vkFields = "name,place,description,start_date,finish_date";
-        /*
-         if (eventUrl.search("event") != -1)
-         {*/
-        eventUrl = eventUrl.slice(6,eventUrl.length);
-        /*    } else {
-         eventUrl = eventUrl.slice(1,eventUrl.length);
-         }*/
+                return;
+            }
 
-        getEventRequest.onload = getEventInfo;
+            infoTabUrl += eventUrl + '&' + items.vkaccess_token;
+            //Открываем окно с инфой
+            chrome.tabs.create({url: infoTabUrl, selected: true});
 
-        getEventRequest.open("GET",
-            "http://api.vk.com/method/groups.getById?" +
-                "v=5.3" +
-                "&group_ids=" + eventUrl +
-                "&fields=" + vkFields
-        );
-        getEventRequest.send(null);
-    });
-}
+        });
+    };
+    /**
+     * Функция получает токен в случае, когда он отсутствует в хранилище
+     */
+    function authListener(authTabId, eventUrl) {
 
-function getEventInfo(event)
-{
-    var vkEvent = JSON.parse(event.target.response);
+        return function tabUpdateListener (tabId, changeInfo) {
+            var vkAccessToken,
+                vkAccessTokenExpiredFlag;
 
-    var vkEventName = vkEvent.response[0].name,
-        description = vkEvent.response[0].description,
-        startTime = timeConverter(vkEvent.response[0].start_date),
-//        location = vkEvent.response[0].place.address,
-        endTime;
-        if (vkEvent.response[0].finish_date === undefined)
-        {
-            endTime = timeConverter(parseInt(vkEvent.response[0].start_date) + 3600);
-        }
-        else
-        {
-            endTime = timeConverter(vkEvent.response[0].finish_date);
+            if (tabId === authTabId && changeInfo.url !== undefined && changeInfo.status === "loading") {
+
+                if (changeInfo.url.indexOf('oauth.vk.com/blank.html') > -1) {
+                    authTabId = null;
+                    chrome.tabs.onUpdated.removeListener(tabUpdateListener);
+
+                    //Получаем значение токена
+                    vkAccessToken = getUrlParameterValue(changeInfo.url, 'access_token');
+
+                    //Выводим ошибку, если токен не получен
+                    if (vkAccessToken === undefined || vkAccessToken.length === undefined) {
+                        showError('vk auth response problem', 'access_token length = 0 or vkAccessToken == undefined');
+                        return;
+                    }
+                    //Получаем отметку об истечении токена
+                    vkAccessTokenExpiredFlag = Number(getUrlParameterValue(changeInfo.url, 'expires_in'));
+
+                    //Выводим ошибку, если токен истек
+                    if (vkAccessTokenExpiredFlag !== 0) {
+                        showError('vk auth response problem', 'vkAccessTokenExpiredFlag != 0' + vkAccessToken);
+                        return;
+                    }
+                    //Сохраняем токен в хранилище и обновляем вкладку
+                    chrome.storage.local.set({'vkaccess_token': vkAccessToken}, function () {
+                        chrome.tabs.update(
+                            tabId,
+                            {
+                                'url' : 'showinfo.html#' + eventUrl + '&' + vkAccessToken,
+                                'active': true
+                            },
+                            function (tab) {}
+                        );
+                    });
+                }
+            }
         };
-    var gLink = "https://www.google.com/calendar/render?" +
-                "action=TEMPLATE" +
-                "&text=" + vkEventName +
-                "&dates=" + startTime + "/" + endTime +
-//                "&location=" + location +
-                "&details=" + description +
-                "&output=xml";
-
-    gLink = encodeURI(gLink.replaceAll("  ", "\n\n")).replaceAll("%20", "+").replaceAll("%2B", "+").substring(0,2000);
-    chrome.tabs.create({ "url": gLink}, function (tab) {});
-}
-
-String.prototype.replaceAll = function(strTarget, strSubString){
-    var strText = this;
-    var intIndexOfMatch = strText.indexOf( strTarget );
-
-    while (intIndexOfMatch != -1){
-        strText = strText.replace( strTarget, strSubString )
-        intIndexOfMatch = strText.indexOf( strTarget );
     }
 
-    return( strText );
+    /**
+     * Парсим полученную ссылку и вытаскиваем из неё нужное значение
+     */
+    function getUrlParameterValue(url, parameterName) {
 
-}
+        var urlParameters = url.substr(url.indexOf("#") + 1),
+            parameterValue = "",
+            index,
+            temp;
 
-function timeConverter(UNIX_timestamp)
-{
+        urlParameters = urlParameters.split("&");
 
-    function pad(str) {
-        str = str.toString();
-        return (str.length == 1) ? '0' + str : str;
+        for (index = 0; index < urlParameters.length; index += 1) {
+            temp = urlParameters[index].split("=");
+
+            if (temp[0] === parameterName) {
+                return temp[1];
+            }
+        }
+
+        return parameterValue;
     }
 
-    var date = new Date(UNIX_timestamp*1000);
-    var year = date.getUTCFullYear(),
-        month = date.getUTCMonth() + 1,
-        day = date.getUTCDate(),
-        hour = date.getUTCHours(),
-        min = date.getUTCMinutes();
-    var time = pad(year) + pad(month) + pad(day) + 'T' + pad(hour) + pad(min) + '00Z';
-    return time;
+    /**
+     * Функция вывода ошибки.
+     */
+    function showError(textToShow, errorToShow) {
+        alert(textToShow + '\n' + errorToShow);
+    }
 }
